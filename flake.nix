96430446d97582146b92660759997f4cb03beb50
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-10-21 00:20:44 +0100

    Added go dev tools

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "your description";
+    description = "P2P in Go";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -9,13 +9,13 @@
     let
         pkgs = import inputs.nixpkgs { inherit system; };
         ROOT = let p = builtins.getEnv "PWD"; in if p == "" then self else p;
-        name = "name of your shell";
+        name = "P2P In Go";
         system = "x86_64-linux";
     in {
         devShells."${system}".default = pkgs.mkShell {
             inherit name ROOT;
 
-            buildInputs = with pkgs; [ go ];
+            buildInputs = with pkgs; [ go gopls ];
 
             shellHook = '''';
         };
